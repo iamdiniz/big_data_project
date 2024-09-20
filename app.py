cf0b88b2f6f4971284535c889e88bd4d1b57cfb8
iamdiniz <guilhermeplays03@gmail.com>
2024-09-20 20:19:05 -0300

    create method - C(RUD)

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ username = "neo4j"
 password = "123456789"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
+def create(tx, name): # tx é uma transação em Cypher, que é a linguagem de consulta do neo4j
+    tx.run("CREATE (n:Person {name: $name})", name=name)
+
+with driver.session() as session: # abrir uma sessão para executar a operação create.
+    session.write_transaction(create, "Guilherme") # session representa a conexão
