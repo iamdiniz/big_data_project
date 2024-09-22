e03ee22d9760c5ff7a91fb492fa581ef1881686b
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-09-22 16:26:30 -0300

    Add function to fetch person by name

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ def create(tx, name): # tx é uma transação em Cypher, que é a linguagem de c
 
 with driver.session() as session: # abrir uma sessão para executar a operação create.
     session.write_transaction(create, "Guilherme") # session representa a conexão
+
+def get_person(tx, name):
+    result = tx.run("MATCH (n:Person {name: $name}) RETURN n.name AS name", name=name)
+    return [record["name"] for record in result]
+
+with driver.session() as session:
+    names = session.read_transaction(get_person, "Guilherme")
+    print(names)
