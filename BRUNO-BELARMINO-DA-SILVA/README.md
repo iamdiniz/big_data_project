6d7369d88995d3bc935cc063913a8b9811f6b1c5
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2024-10-07 22:11:22 -0300

    Atividade de python concluida - Bruno

---
+++ BRUNO-BELARMINO-DA-SILVA/README.md
@@ -0,0 +1,2 @@
+# Nome: Bruno Belarmino da Silva
+## Matricula: 24010424
--- app.py
+++ app.py
@@ -5,11 +5,11 @@ username = "neo4j"
 password = "123456789"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def create(tx, name): # tx é uma transação em Cypher, que é a linguagem de consulta do neo4j
-    tx.run("CREATE (n:Person {name: $name})", name=name)
+# def create(tx, name): # tx é uma transação em Cypher, que é a linguagem de consulta do neo4j
+#     tx.run("CREATE (n:Person {name: $name})", name=name)
 
-with driver.session() as session: # abrir uma sessão para executar a operação create.
-    session.write_transaction(create, "Darlan") # session representa a conexão
+# with driver.session() as session: # abrir uma sessão para executar a operação create.
+#     session.write_transaction(create, "Darlan") # session representa a conexão
 
 def get_person(tx, name):
     result = tx.run("MATCH (n:Person {name: $name}) RETURN n.name AS name", name=name)
@@ -27,18 +27,18 @@ with driver.session() as session:
     session.write_transaction(update_person, "Guilherme", "Diniz")
     print("Nome atualizado!")
 
-def delete_person(tx, name):
-    tx.run("MATCH (n:Person {name: $name}) "
-           "DELETE n", name=name)
+# def delete_person(tx, name):
+#     tx.run("MATCH (n:Person {name: $name}) "
+#            "DELETE n", name=name)
 
-with driver.session() as session:
-    session.write_transaction(delete_person, "Diniz")
-    print("Pessoa deletada!")
+# with driver.session() as session:
+#     session.write_transaction(delete_person, "Diniz")
+#     print("Pessoa deletada!")
 
-def create_friendship(tx, person1_name, person2_name):
-    tx.run("MATCH (a:Person {name: $person1_name}), (b:Person {name: $person2_name}) "
-           "CREATE (a)-[:AMIGO_DE]->(b)", person1_name=person1_name, person2_name=person2_name)
+# def create_friendship(tx, person1_name, person2_name):
+#     tx.run("MATCH (a:Person {name: $person1_name}), (b:Person {name: $person2_name}) "
+#            "CREATE (a)-[:AMIGO_DE]->(b)", person1_name=person1_name, person2_name=person2_name)
 
-with driver.session() as session:
-    session.write_transaction(create_friendship, "Darlan", "Eduarda")
-    print("Relacionamento AMIGO_DE criado!")
+# with driver.session() as session:
+#     session.write_transaction(create_friendship, "Darlan", "Eduarda")
+#     print("Relacionamento AMIGO_DE criado!")
